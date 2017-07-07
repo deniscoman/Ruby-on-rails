1f2e0524bf7c60ab3b45ce756ddfea32f1cc8e6f
Denis Coman <denis.coman13@gmail.com>
2017-07-07 11:07:48 +0300

    Added goodbye acction

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: "Hello, World!"
+  	render html: "¡Hola, Mundo!"
+  end
+
+  def goodbye
+  	render html: "Goodbye, World"
   end	
 end
